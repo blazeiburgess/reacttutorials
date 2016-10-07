549eced3df47dafd1fc71161de23ad0fe532e6ed
blazeiburgess <blaze.i.burgess@gmail.com>
2016-10-07 19:58:31 -0400

    Added comment lists and forms, modified comment box to display both

--- official/public/scripts/commentBox.js
+++ official/public/scripts/commentBox.js
@@ -2,9 +2,27 @@ var CommentBox = React.createClass({
   render: function () {
     return (
       <div className="commentBox">
-	Hello, world! I am a CommentBox.
+	<h1>Comments</h1>
+	<CommentList />
+	<CommentForm />
       </div>
     )
+  }
+});
+
+var CommentList = React.createClass({
+  render function () {
+    <div className="commentList">
+      Hello, world! I am a CommentList.
+    </div>
+  }
+});
+
+var CommentForm = React.createClass({
+  render function () {
+    <div className="commentForm">
+      Hello, world! I am a CommentForm.
+    </div>
   }
 });
 
