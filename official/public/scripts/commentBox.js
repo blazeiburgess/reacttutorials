54c6b92f532ffe48d20bf141471e833ace959e04
blazeiburgess <blaze.i.burgess@gmail.com>
2016-10-07 20:00:01 -0400

    Fixed typos and gave anonymous function jsx return value

--- official/public/scripts/commentBox.js
+++ official/public/scripts/commentBox.js
@@ -11,18 +11,22 @@ var CommentBox = React.createClass({
 });
 
 var CommentList = React.createClass({
-  render function () {
-    <div className="commentList">
-      Hello, world! I am a CommentList.
-    </div>
+  render: function () {
+    return (
+      <div className="commentList">
+	Hello, world! I am a CommentList.
+      </div>
+    )
   }
 });
 
 var CommentForm = React.createClass({
-  render function () {
-    <div className="commentForm">
-      Hello, world! I am a CommentForm.
-    </div>
+  render: function () {
+    return (
+      <div className="commentForm">
+	Hello, world! I am a CommentForm.
+      </div>
+    )
   }
 });
 
