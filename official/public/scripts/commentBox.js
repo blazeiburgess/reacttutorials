a588975c84dfa7acc44d907f494b35a8d458d4b9
blazeiburgess <blaze.i.burgess@gmail.com>
2016-10-09 10:32:53 -0400

    Change form to handle user input

--- official/public/scripts/commentBox.js
+++ official/public/scripts/commentBox.js
@@ -51,11 +51,30 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+  getInitialState: function () {
+    return {author: '', text: ''};
+  },
+  handleAuthorChange: function(e) {
+    this.setState({author: e.target.value});
+  },
+  handleTextChange: function (e) {
+    this.setState({text: e.target.value});
+  },
   render: function () {
     return (
       <form className="commentForm">
-	<input type="text"  placeholder="You name" />
-	<input type="text" placeholder="Say something..." />
+	<input 
+	  type="text"  
+	  placeholder="You name" 
+	  value={this.state.author} 
+	  onChange={this.hndleAuthorChange} 
+	/>
+	<input 
+	  type="text" 
+	  placeholder="Say something..." 
+	  value={this.state.text}
+	  onChange={this.handleTextChange}
+	/>
 	<input type="submit" value="Post" /> 
       </form>
     )
